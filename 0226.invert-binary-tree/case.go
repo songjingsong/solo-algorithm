0d09da49d070af2da1a25726dfd9c48c4ca980f9
unknown <ives.song@blackshark.com>
2020-04-26 16:40:02 +0800

    add 0226

---
+++ 0226.invert-binary-tree/case.go
@@ -0,0 +1,17 @@
+package case0226
+
+func invertTree(root *TreeNode) *TreeNode {
+	if root==nil{
+		return root
+	}
+	root.Left,root.Right=root.Right,root.Left
+	invertTree(root.Left)
+	invertTree(root.Right)
+	return root
+}
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
